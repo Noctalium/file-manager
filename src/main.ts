a52b74f4e35af2fd5edb49b0ca7b1067d7b33952
Noctalium <noctaflygame@gmail.com>
2020-08-06 19:14:45 +0200

    Added .env support for app port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import "reflect-metadata";
 
+require('dotenv').config()
+
 async function fileManager() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3000);
 }
 
 fileManager();
